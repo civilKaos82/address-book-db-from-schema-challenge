1ff2e8baf641f792d796a014a954c09dce948c30
Torey Hickman <torey@toreyhickman.com>
2016-06-09 16:49:21 -0500

    Contact can interact with a database

--- contact.rb
+++ contact.rb
@@ -1,4 +1,83 @@
 class Contact
-  # TODO: implement the class corresponding to the contacts table
+  attr_reader :id, :created_at, :updated_at
+  attr_accessor :name, :phone, :email
+
+  def initialize(args = {})
+    assign_attributes(args)
+  end
+
+  def destroy
+    $db.execute("DELETE FROM contacts WHERE id = ?", self.id)
+  end
+
+  def save
+    persisted? ? update : insert
+    return self
+  end
+
+  def self.count
+    $db.get_first_value("SELECT COUNT() FROM contacts;")
+  end
+
+  def self.all
+    contacts_from($db.execute("SELECT * FROM contacts;"))
+  end
+
+  def self.where(conditions)
+    contacts_from($db.execute("SELECT * FROM contacts WHERE #{build_conditions_with_placeholders(conditions.keys)};", *conditions.values))
+  end
+
+  def self.find(id)
+    contacts_from($db.execute("SELECT * FROM contacts WHERE id = ? LIMIT 1", id)).first
+  end
+
+  private
+  attr_writer :id
+
+  def created_at=(date)
+    @created_at = date_from(date)
+  end
+
+  def updated_at=(date)
+    @updated_at = date_from(date)
+  end
+
+  def date_from(date)
+    return date if date.kind_of? Date
+    DateTime.parse(date)
+  end
+
+  def assign_attributes(args = {})
+    self.id         = args["id"]         if args["id"]
+    self.name       = args["name"]       if args["name"]
+    self.phone      = args["phone"]      if args["phone"]
+    self.email      = args["email"]      if args["email"]
+    self.created_at = args["created_at"] if args["created_at"]
+    self.updated_at = args["updated_at"] if args["updated_at"]
+  end
+
+  def persisted?
+    !id.nil?
+  end
+
+  def insert
+    $db.execute("INSERT INTO contacts (name, phone, email, created_at, updated_at) VALUES (?, ?, ?, DATETIME('now'), DATETIME('now'));", self.name, self.phone, self.email)
+    assign_attributes($db.execute("SELECT * FROM contacts WHERE id = ? LIMIT 1", $db.last_insert_row_id).first)
+  end
+
+  def update
+    $db.execute("UPDATE contacts SET name = ?, phone = ?, email = ?, updated_at = DATETIME('now') WHERE id = ?", self.name, self.phone, self.email, self.id)
+    assign_attributes($db.execute("SELECT * FROM contacts WHERE id = ? LIMIT 1", self.id).first)
+  end
+
+  def self.contacts_from(results)
+    results.map { |group_data| new(group_data) }
+  end
+
+  def self.build_conditions_with_placeholders(columns)
+    columns.map { |column| "#{column} = ?" }.join(" AND ")
+  end
 end
+
+
 
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -1,97 +1,206 @@
 require_relative '../config'
 require_relative '../factories'
 require_relative '../contact'
-require_relative '../group'
+require_relative '../contact'
 
 describe Contact do
-  before(:each) do
-    $db.transaction
+  it "has a readable and writeable name" do
+    contact = Contact.new("name" => "Daniel")
+    expect(contact.name).to eq "Daniel"
+    contact.name = "Ben"
+    expect(contact.name).to eq "Ben"
   end
 
-  after(:each) do
-    $db.rollback
+  it "has a readable and writeable phone number" do
+    contact = Contact.new("phone" => "419-555-1928")
+    expect(contact.phone).to eq "419-555-1928"
+    contact.phone = "419-555-3948"
+    expect(contact.phone).to eq "419-555-3948"
   end
 
-  pending "#save" do
-    pending "on a new record" do
-      let(:contact) { Contact.new }
+  it "has a readabel and writeable email address" do
+    contact = Contact.new("email" => "casey@gmail.com")
+    expect(contact.email).to eq "casey@gmail.com"
+    contact.email = "annie@gmail.com"
+    expect(contact.email).to eq "annie@gmail.com"
+  end
 
-      pending "sets id after saving" do
-        expect(contact.id).to be_nil
-        contact.save
-        expect(contact.id).not_to be_nil
+  it "has a readable id" do
+    contact = Contact.new("id" => 1)
+    expect(contact.id).to eq 1
+  end
+
+  it "has a readable created_at time" do
+    contact = Contact.new("created_at" => "2016-06-06")
+    expect(contact.created_at).to eq Date.parse("2016-06-06")
+  end
+
+  it "has a readable updated_at time" do
+    contact = Contact.new("updated_at" => "2016-06-07")
+    expect(contact.updated_at).to eq Date.parse("2016-06-07")
+  end
+
+  describe "interacting with the database" do
+    describe "retrieving records from the database" do
+      before(:each) do
+        $db.execute(
+          <<-SQL_INSERT_STATEMENT
+          INSERT INTO contacts
+            (name, phone, email, created_at, updated_at)
+          VALUES
+            ("Andrew", "419-555-1234", "andrew@gmail.com", DATETIME("now"), DATETIME("now")),
+            ("Jared", "419-555-9876", "jared@gmail.com", DATETIME("now"), DATETIME("now")),
+            ("Beth", "419-555-9876", "beth@gmail.com", DATETIME("now"), DATETIME("now"));
+          SQL_INSERT_STATEMENT
+        )
+      end
+
+      it "counts the number of records in the database" do
+        number_of_contacts_in_db = $db.get_first_value('SELECT COUNT(*) FROM contacts;')
+        expect(Contact.count).to equal number_of_contacts_in_db
+      end
+
+      it "selects all the contacts in the database" do
+        number_of_contacts_in_db = $db.get_first_value('SELECT COUNT(*) FROM contacts;')
+        expect(Contact.all.count).to equal number_of_contacts_in_db
+      end
+
+      it "selects all the contacts matching given conditions" do
+        number_of_contacts_named_jared = $db.get_first_value("SELECT COUNT(*) FROM contacts WHERE name = 'Jared';")
+        expect(Contact.where(name: "Jared").count).to eq number_of_contacts_named_jared
+      end
+
+      it "selects a single record by id" do
+        andrew_id = $db.get_first_value("SELECT id FROM contacts WHERE name = 'Andrew';")
+        contact = Contact.find(andrew_id)
+        expect(contact.name).to eq "Andrew"
+      end
+
+      it "returns nothing if it can't find a record by id" do
+        expect(Contact.find(0)).to be_nil
       end
     end
 
-    pending "on an existing record" do
-      let(:contact) do
-        Contact.find(create_contact(company: 'Initech'))
+    describe "removing data from the database" do
+      before(:each) do
+        $db.execute(
+          <<-SQL_INSERT_STATEMENT
+          INSERT INTO contacts
+            (name, phone, email, created_at, updated_at)
+          VALUES
+            ("Caroline", "419-555-1234", "caroline@gmail.com", DATETIME("now"), DATETIME("now"))
+          SQL_INSERT_STATEMENT
+        )
       end
 
-      pending "updates the other columns" do
-        expect(contact.company).to eq('Initech')
-        contact.company = 'Intertrode'
-        contact.save
-        expect(contact.company).to eq('Intertrode')
+      it "removes the record associated with the contact from the database" do
+        count_of_contacts = $db.get_first_value("SELECT COUNT() FROM contacts;")
+
+        contact = Contact.new($db.execute('SELECT * FROM contacts LIMIT 1').first)
+        contact.destroy
+
+        updated_count_of_contacts = $db.get_first_value("SELECT COUNT() FROM contacts;")
+        expect(updated_count_of_contacts).to eq (count_of_contacts - 1)
       end
     end
-  end
 
-  pending ".find" do
-    let(:id) { create_contact(first_name: 'Nobody') }
+    describe "writing to the database" do
+      context "when the contact has not been persisted" do
+        it "inserts a new record into the database" do
+          count_of_contacts = $db.get_first_value("SELECT COUNT() FROM contacts;")
 
-    pending "finds record by ID" do
-      contact = Contact.find(id)
-      expect(contact).to_not be_nil
-      expect(contact.id).to eq(id)
+          contact = Contact.new("name" => "Sasha")
+          contact.save
+
+          updated_count_of_contacts = $db.get_first_value("SELECT COUNT() FROM contacts;")
+          expect(updated_count_of_contacts).to eq (count_of_contacts + 1)
+        end
+
+        it "updates the contact with the id from the database" do
+          contact = Contact.new("name" => "Dusty")
+          contact.save
+
+          expect(contact.id).to eq $db.last_insert_row_id
+        end
+
+        it "updates the contact with the created_at time from the database" do
+          contact = Contact.new("name" => "Tom")
+          contact.save
+
+          expected_created_at = DateTime.parse($db.get_first_value("SELECT created_at FROM contacts WHERE id = ? LIMIT 1", $db.last_insert_row_id))
+          expect(contact.created_at).to eq expected_created_at
+        end
+
+        it "updates the contact with the updated_at time from the database" do
+          contact = Contact.new("name" => "Parth")
+          contact.save
+
+          expected_updated_at = DateTime.parse($db.get_first_value("SELECT updated_at FROM contacts WHERE id = ? LIMIT 1", $db.last_insert_row_id))
+          expect(contact.updated_at).to eq expected_updated_at
+        end
+      end
+
+      context "when the contact has already been persisted" do
+        before(:each) do
+          $db.execute(
+            <<-SQL_INSERT_STATEMENT
+            INSERT INTO contacts
+              (name, phone, email, created_at, updated_at)
+            VALUES
+              ("Paige", "419-555-1234", "paige@gmail.com", DATETIME("now"), DATETIME("now"))
+            SQL_INSERT_STATEMENT
+          )
+        end
+
+        it "updates changed attributes in the database" do
+          contact = Contact.new($db.execute("SELECT * FROM contacts LIMIT 1").first)
+
+          contact_name_in_db = $db.get_first_value("SELECT name FROM contacts WHERE id = ?", contact.id)
+          expect(contact_name_in_db).to eq "Paige"
+
+          contact.name = "Ariel"
+          contact.save
+
+          updated_contact_name_in_db = $db.get_first_value("SELECT name FROM contacts WHERE id = ?", contact.id)
+          expect(updated_contact_name_in_db).to eq "Ariel"
+        end
+
+        it "updates the updated_at time in the database" do
+          sleep(1)
+          contact = Contact.new($db.execute("SELECT * FROM contacts LIMIT 1").first)
+          original_updated_at_in_db = $db.get_first_value("SELECT updated_at FROM contacts WHERE id = ?", contact.id)
+
+          contact.name = "Nate"
+          contact.save
+
+          updated_updated_at_in_db = $db.get_first_value("SELECT updated_at FROM contacts WHERE id = ?", contact.id)
+          expect(updated_updated_at_in_db).to_not eq original_updated_at_in_db
+        end
+
+        it "updates the contact with the updated_at time from the database" do
+          sleep(1)
+          contact = Contact.new($db.execute("SELECT * FROM contacts LIMIT 1").first)
+          contact.name = "Oliver"
+          contact.save
+
+          expected_updated_at = DateTime.parse($db.get_first_value("SELECT updated_at FROM contacts WHERE id = ?", contact.id))
+          expect(contact.updated_at).to eq expected_updated_at
+        end
+      end
     end
 
-    pending "returns nil if the record is not found" do
-      contact = Contact.find(-100)
-      expect(contact).to be_nil
-    end
-  end
-
-  pending "#destroy" do
-    let(:contact) { Contact.find(create_contact(first_name: 'Bob')) }
-
-    pending "removes the record" do
-      contact.destroy
-      expect(Contact.find(contact.id)).to be_nil
-    end
-  end
-
-  pending ".all" do
-    let!(:alice_id) { create_contact(first_name: 'Alice') }
-    let!(:bob_id) { create_contact(first_name: 'Bob') }
-    let(:contacts) { Contact.all }
-
-    pending "returns an array of contacts" do
-      expect(contacts).to be_an(Array)
-      expect(contacts.first).to be_a(Contact)
+    before(:each) do
+      # Start each test with no data in the tables.
+      # We'll add any data we need before each specific test.
+      $db.transaction
+      $db.execute("DELETE FROM contacts")
+      $db.execute("DELETE FROM contacts")
     end
 
-    pending "returns all contacts" do
-      ids = contacts.map(&:id)
-      expect(ids).to include(alice_id)
-      expect(ids).to include(bob_id)
-    end
-  end
-
-  pending ".where" do
-    let!(:alice_id) { create_contact(first_name: 'Alice') }
-    let!(:bob_id) { create_contact(first_name: 'Bob') }
-    let(:contacts) { Contact.where("first_name = 'Alice'") }
-
-    pending "returns an array of contacts" do
-      expect(contacts).to be_an(Array)
-      expect(contacts.first).to be_a(Contact)
-    end
-
-    pending "includes only matching records" do
-      ids = contacts.map(&:id)
-      expect(ids).to include(alice_id)
-      expect(ids).not_to include(bob_id)
+    # Undo any changes to the database after each test.
+    after(:each) do
+      $db.rollback
     end
   end
 end
+
