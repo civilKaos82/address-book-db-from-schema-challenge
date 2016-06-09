dfd4c7ef4d144c8e3abcb4bb3c189a7c63212a51
Torey Hickman <torey@toreyhickman.com>
2016-06-09 14:32:03 -0500

    Group can interact with a database

--- group.rb
+++ group.rb
@@ -1,5 +1,80 @@
 class Group
-  # TODO: implement the class that corresponds to the groups table
+  attr_reader :id, :created_at, :updated_at
+  attr_accessor :name
+
+  def initialize(args = {})
+    assign_attributes(args)
+  end
+
+  def destroy
+    $db.execute("DELETE FROM groups WHERE id = ?", self.id)
+  end
+
+  def save
+    persisted? ? update : insert
+    return self
+  end
+
+  def self.count
+    $db.get_first_value("SELECT COUNT() FROM groups;")
+  end
+
+  def self.all
+    groups_from($db.execute("SELECT * FROM groups;"))
+  end
+
+  def self.where(conditions)
+    groups_from($db.execute("SELECT * FROM groups WHERE #{build_conditions_with_placeholders(conditions.keys)};", *conditions.values))
+  end
+
+  def self.find(id)
+    groups_from($db.execute("SELECT * FROM groups WHERE id = ? LIMIT 1", id)).first
+  end
+
+  private
+  attr_writer :id
+
+  def created_at=(date)
+    @created_at = date_from(date)
+  end
+
+  def updated_at=(date)
+    @updated_at = date_from(date)
+  end
+
+  def date_from(date)
+    return date if date.kind_of? Date
+    DateTime.parse(date)
+  end
+
+  def assign_attributes(args = {})
+    self.id         = args["id"]         if args["id"]
+    self.name       = args["name"]       if args["name"]
+    self.created_at = args["created_at"] if args["created_at"]
+    self.updated_at = args["updated_at"] if args["updated_at"]
+  end
+
+  def persisted?
+    !id.nil?
+  end
+
+  def insert
+    $db.execute("INSERT INTO groups (name, created_at, updated_at) VALUES (?, DATETIME('now'), DATETIME('now'));", self.name)
+    assign_attributes($db.execute("SELECT * FROM groups WHERE id = ? LIMIT 1", $db.last_insert_row_id).first)
+  end
+
+  def update
+    $db.execute("UPDATE groups SET name = ?, updated_at = DATETIME('now') WHERE id = ?", self.name, self.id)
+    assign_attributes($db.execute("SELECT * FROM groups WHERE id = ? LIMIT 1", self.id).first)
+  end
+
+  def self.groups_from(results)
+    results.map { |group_data| new(group_data) }
+  end
+
+  def self.build_conditions_with_placeholders(columns)
+    columns.map { |column| "#{column} = ?" }.join(" AND ")
+  end
 end
 
 
--- spec/group_spec.rb
+++ spec/group_spec.rb
@@ -1,111 +1,190 @@
 require_relative '../config'
-require_relative '../factories'
-require_relative '../contact'
 require_relative '../group'
+require_relative '../contact'
 
 describe Group do
-  before(:each) do
-    $db.transaction
+  it "has a readable and writeable name" do
+    group = Group.new("name" => "Otters")
+    expect(group.name).to eq "Otters"
+    group.name = "Nighthawks"
+    expect(group.name).to eq "Nighthawks"
   end
 
-  after(:each) do
-    $db.rollback
+  it "has a readable id" do
+    group = Group.new("id" => 1)
+    expect(group.id).to eq 1
   end
 
-  pending "#save" do
-    pending "on a new record" do
-      let(:group) { Group.new }
+  it "has a readable created_at time" do
+    group = Group.new("created_at" => "2016-06-06")
+    expect(group.created_at).to eq Date.parse("2016-06-06")
+  end
 
-      pending "sets id after saving" do
-        expect(group.id).to be_nil
-        group.save
-        expect(group.id).not_to be_nil
+  it "has a readable updated_at time" do
+    group = Group.new("updated_at" => "2016-06-07")
+    expect(group.updated_at).to eq Date.parse("2016-06-07")
+  end
+
+  describe "interacting with the database" do
+    describe "retrieving records from the database" do
+      before(:each) do
+        $db.execute(
+          <<-SQL_INSERT_STATEMENT
+          INSERT INTO groups
+            (name, created_at, updated_at)
+          VALUES
+            ("Otters", DATETIME("now"), DATETIME("now")),
+            ("Nighthawks", DATETIME("now"), DATETIME("now"));
+          SQL_INSERT_STATEMENT
+        )
+      end
+
+      it "counts the number of records in the database" do
+        number_of_groups_in_db = $db.get_first_value('SELECT COUNT(*) FROM groups;')
+        expect(Group.count).to equal number_of_groups_in_db
+      end
+
+      it "selects all the groups in the database" do
+        number_of_groups_in_db = $db.get_first_value('SELECT COUNT(*) FROM groups;')
+        expect(Group.all.count).to equal number_of_groups_in_db
+      end
+
+      it "selects all the groups matching given conditions" do
+        number_of_groups_named_nighthawks = $db.get_first_value("SELECT COUNT(*) FROM groups WHERE name = 'Nighthawks';")
+        expect(Group.where(name: "Nighthawks").count).to eq number_of_groups_named_nighthawks
+      end
+
+      it "selects a single record by id" do
+        nighthawks_id = $db.get_first_value("SELECT id FROM groups WHERE name = 'Nighthawks';")
+        group = Group.find(nighthawks_id)
+        expect(group.name).to eq "Nighthawks"
+      end
+
+      it "returns nothing if it can't find a record by id" do
+        expect(Group.find(0)).to be_nil
       end
     end
 
-    pending "on an existing record" do
-      let(:group) { Group.find(create_group(name: 'Peeps')) }
-
-      pending "updates the other columns" do
-        expect(group.name).to eq('Peeps')
-        group.name = 'Moar Peeps'
-        group.save
-        expect(group.name).to eq('Moar Peeps')
-      end
-    end
-  end
-
-  pending ".find" do
-    let(:id) { create_group(name: 'Boots') }
-
-    pending "finds record by ID" do
-      group = Group.find(id)
-      expect(group).to_not be_nil
-      expect(group.id).to eq(id)
-    end
-
-    pending "returns nil if the record is not found" do
-      group = Group.find(-100)
-      expect(group).to be_nil
-    end
-  end
-
-  pending "#destroy" do
-    let(:group) { Group.find(create_group(name: 'Bob')) }
-
-    pending "removes the record" do
-      group.destroy
-      expect(Group.find(group.id)).to be_nil
-    end
-
-    pending "cascading delete" do
-      let!(:contact) do
-        record = Contact.find(create_contact(first_name: 'Bob'))
-        record.add_group(group)
-        record
+    describe "removing data from the database" do
+      before(:each) do
+        $db.execute(
+          <<-SQL_INSERT_STATEMENT
+          INSERT INTO groups
+            (name, created_at, updated_at)
+          VALUES
+            ("Otters", DATETIME("now"), DATETIME("now"))
+          SQL_INSERT_STATEMENT
+        )
       end
 
-      pending "removes the group" do
-        expect(contact.groups).not_to be_empty
+      it "removes the record associated with the group from the database" do
+        count_of_groups = $db.get_first_value("SELECT COUNT() FROM groups;")
+
+        group = Group.new($db.execute('SELECT * FROM groups LIMIT 1').first)
         group.destroy
-        expect(contact.groups).to be_empty
+
+        updated_count_of_groups = $db.get_first_value("SELECT COUNT() FROM groups;")
+        expect(updated_count_of_groups).to eq (count_of_groups - 1)
       end
     end
+
+    describe "writing to the database" do
+      context "when the group has not been persisted" do
+        it "inserts a new record into the database" do
+          count_of_groups = $db.get_first_value("SELECT COUNT() FROM groups;")
+
+          group = Group.new("name" => "Nighthawks")
+          group.save
+
+          updated_count_of_groups = $db.get_first_value("SELECT COUNT() FROM groups;")
+          expect(updated_count_of_groups).to eq (count_of_groups + 1)
+        end
+
+        it "updates the group with the id from the database" do
+          group = Group.new("name" => "Nighthawks")
+          group.save
+
+          expect(group.id).to eq $db.last_insert_row_id
+        end
+
+        it "updates the group with the created_at time from the database" do
+          group = Group.new("name" => "Nighthawks")
+          group.save
+
+          expected_created_at = DateTime.parse($db.get_first_value("SELECT created_at FROM groups WHERE id = ? LIMIT 1", $db.last_insert_row_id))
+          expect(group.created_at).to eq expected_created_at
+        end
+
+        it "updates the group with the updated_at time from the database" do
+          group = Group.new("name" => "Nighthawks")
+          group.save
+
+          expected_updated_at = DateTime.parse($db.get_first_value("SELECT updated_at FROM groups WHERE id = ? LIMIT 1", $db.last_insert_row_id))
+          expect(group.updated_at).to eq expected_updated_at
+        end
+      end
+
+      context "when the group has already been persisted" do
+        before(:each) do
+          $db.execute(
+            <<-SQL_INSERT_STATEMENT
+            INSERT INTO groups
+              (name, created_at, updated_at)
+            VALUES
+              ("Otters", DATETIME("now"), DATETIME("now"))
+            SQL_INSERT_STATEMENT
+          )
+        end
+
+        it "updates changed attributes in the database" do
+          group = Group.new($db.execute("SELECT * FROM groups LIMIT 1").first)
+
+          group_name_in_db = $db.get_first_value("SELECT name FROM groups WHERE id = ?", group.id)
+          expect(group_name_in_db).to eq "Otters"
+
+          group.name = "Nighthawks"
+          group.save
+
+          updated_group_name_in_db = $db.get_first_value("SELECT name FROM groups WHERE id = ?", group.id)
+          expect(updated_group_name_in_db).to eq "Nighthawks"
+        end
+
+        it "updates the updated_at time in the database" do
+          sleep(1)
+          group = Group.new($db.execute("SELECT * FROM groups LIMIT 1").first)
+          original_updated_at_in_db = $db.get_first_value("SELECT updated_at FROM groups WHERE id = ?", group.id)
+
+          group.name = "Nighthawks"
+          group.save
+
+          updated_updated_at_in_db = $db.get_first_value("SELECT updated_at FROM groups WHERE id = ?", group.id)
+          expect(updated_updated_at_in_db).to_not eq original_updated_at_in_db
+        end
+
+        it "updates the group with the updated_at time from the database" do
+          sleep(1)
+          group = Group.new($db.execute("SELECT * FROM groups LIMIT 1").first)
+          group.name = "Nighthawks"
+          group.save
+
+          expected_updated_at = DateTime.parse($db.get_first_value("SELECT updated_at FROM groups WHERE id = ?", group.id))
+          expect(group.updated_at).to eq expected_updated_at
+        end
+      end
+    end
+
+    before(:each) do
+      # Start each test with no data in the tables.
+      # We'll add any data we need before each specific test.
+      $db.transaction
+      $db.execute("DELETE FROM groups")
+      $db.execute("DELETE FROM contacts")
+    end
+
+    # Undo any changes to the database after each test.
+    after(:each) do
+      $db.rollback
+    end
   end
-
-  pending ".all" do
-    let!(:firefly_id) { create_group(name: 'Fireflies') }
-    let!(:coyote_id) { create_group(name: 'Coyotes') }
-    let(:groups) { Group.all }
-
-    pending "returns an array of groups" do
-      expect(groups).to be_an(Array)
-      expect(groups.first).to be_a(Group)
-    end
-
-    pending "returns all groups" do
-      ids = groups.map(&:id)
-      expect(ids).to include(firefly_id)
-      expect(ids).to include(coyote_id)
-    end
-  end
-
-  pending ".where" do
-    let!(:firefly_id) { create_group(name: 'Fireflies') }
-    let!(:coyote_id) { create_group(name: 'Coyotes') }
-    let(:groups) { Group.where("name = 'Fireflies'") }
-
-    pending "returns an array of groups" do
-      expect(groups).to be_an(Array)
-      expect(groups.first).to be_a(Group)
-    end
-
-    pending "includes only matching records" do
-      ids = groups.map(&:id)
-      expect(ids).to include(firefly_id)
-      expect(ids).not_to include(coyote_id)
-    end
-  end
-
 end
 
